d54a1f00923d597ea6823595c4be2d58251c0bf9
Damian Roiz <damianx989@gmail.com>
2022-06-02 18:26:53 -0400

    Running API Test with DotEnv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
 
-const MONGO_URL = 'mongodb+srv://nasa-api:BfTdRAVhskzvyL7W@nasacluster.lhrgm.mongodb.net/nasa?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connecting ready!');
